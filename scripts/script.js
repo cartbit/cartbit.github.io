c69e7c93c03e7ab9fc0848832a101894ba9d0d08
cartbit <rizz1000fps@gmail.com>
2024-11-27 11:57:49 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,7 @@ function loadData() {
     var url = "http://localhost:6543";
     xhr.open("GET", url, true)
     xhr.onload = function () {
-        if (this.status === 200) {
             handleData(JSON.parse(this.responseText));
-        }
-        else
-            alert("Something Went Wrong! Error: " + this.status);
     }
     xhr.send();
 }
