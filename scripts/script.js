c57ed0baceb21fca5d3579a6b4a873de706a2569
cartbit <rizz1000fps@gmail.com>
2024-11-27 15:32:24 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 
 function loadData() {
     var xhr = new XMLHttpRequest;
-    var url = "https://localhost:6543";
+    var url = "http://localhost:6543";
     xhr.open("GET", url, true)
     xhr.onload = function () {
             handleData(JSON.parse(this.responseText));
