24a431dbde56900d2292101e3cf710299e105b23
cartbit <rizz1000fps@gmail.com>
2024-11-27 11:35:54 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,16 +16,5 @@ function loadData() {
 }
 
 function handleData(data) {
-    var url = data.message;
-    var regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+\-?\w+)\/.+/g;
-    var breedName = regex.exec(url);
-    var name = toTitleCase(breedName[1]).toUpperCase();
-    document.querySelector(".img-container").innerHTML = "<img class='page-img' alt='" + name + "' src='" + url + "'/>";
-    document.querySelector(".img-info").innerHTML = "<p>" + name + "</p>";
-}
-
-function toTitleCase(str) {
-    return str.replace(/\w\S*/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toUpperCase();
-    });
+   alert(data);
 }
